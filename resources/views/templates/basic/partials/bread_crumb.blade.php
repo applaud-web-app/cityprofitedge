efedabd65cd1aec31ec9c0d9ebaf104d53374996
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-01-16 11:20:09 +0530

    Update bread_crumb.blade.php

--- resources/views/templates/basic/partials/bread_crumb.blade.php
+++ resources/views/templates/basic/partials/bread_crumb.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center">
             @if($user)
                 <div class="col-lg-12 col-12">
-                    <h5 class="text-white text-end mb-0">Welcome to <span class="text--base">{{$user->firstname.' '.$user->lastname}}</span> </h5>
+                    <h5 class="text-white text-start mb-0">Welcome to <span class="text--base">{{$user->firstname.' '.$user->lastname}}</span> </h5>
                 </div>
             @endif
             <div class="col-lg-6 text-center">
