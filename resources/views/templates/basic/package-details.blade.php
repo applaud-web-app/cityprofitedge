b279fa660c6751edf341e3ca34d5f3119bbdf0a6
devansh9012 <tdevansh099@gmail.com>
2024-01-22 16:26:56 +0530

    Update package-details.blade.php

--- resources/views/templates/basic/package-details.blade.php
+++ resources/views/templates/basic/package-details.blade.php
@@ -126,7 +126,7 @@
     </div>
 @else
     <div class="modal fade cmn--modal" id="chooseModal">
-        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header"> 
                     <h5 class="modal-title method-name">@lang('Kindly login before proceeding with the request callback')</h5>
