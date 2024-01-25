ed174bb8c0742fa557b5523cfab8c323c8ec2fe4
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-01-25 17:36:23 +0530

    Update TradeController.php

--- app/Http/Controllers/Admin/TradeController.php
+++ app/Http/Controllers/Admin/TradeController.php
@@ -26,41 +26,6 @@ class TradeController extends Controller
     }
     public function tradePosition(){
         $pageTitle = 'Trade Position';
-        
-
-        // Initialise.
-        $kite = new KiteConnect("your_api_key");
-
-        // Assuming you have obtained the `request_token`
-        // after the auth flow redirect by redirecting the
-        // user to $kite->login_url()
-        try {
-            $user = $kite->generateSession("request_token_obtained", "your_api_secret");
-            echo "Authentication successful. \n";
-            print_r($user);
-            $kite->setAccessToken($user->access_token);
-        } catch(Exception $e) {
-            echo "Authentication failed: ".$e->getMessage();
-            throw $e;
-        }
-
-        echo $user->user_id." has logged in";
-
-        // Get the list of positions.
-        echo "Positions: \n";
-        print_r($kite->getPositions());
-
-        // Place order.
-        $order = $kite->placeOrder("regular", [
-            "tradingsymbol" => "INFY",
-            "exchange" => "NSE",
-            "quantity" => 1,
-            "transaction_type" => "BUY",
-            "order_type" => "MARKET",
-            "product" => "NRML"
-        ]);
-
-        echo "Order id is ".$order->order_id;
         return view('admin.trade.trade-position', compact('pageTitle'));
      
     }
