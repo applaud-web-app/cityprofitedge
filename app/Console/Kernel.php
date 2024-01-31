76cdaa090d9039c2fc96b749cb65fc1690e170b6
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-01-31 17:36:32 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('oms-config:command')->everyMinute()->between('8:00','20:00');
+        $schedule->command('oms-config:command')->everyMinute()->between('8:00','20:00')->sendOutputTo('command1_output.log');
     }
 
     /**
