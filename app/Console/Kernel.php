222b40d19c9f796a63f1b0add31caba5c92a52ac
devansh9012 <tdevansh099@gmail.com>
2024-02-02 10:06:29 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('oms-config:command')->everyMinute()->between('8:00','20:00')->sendOutputTo('command1_output.log');
-        $schedule->command('angel_instrument:daily_update')->dailyAt('10:05')->sendOutputTo('command2_output.log');
+        $schedule->command('angel_instrument:daily_update')->dailyAt('10:10')->sendOutputTo('command2_output.log');
         // $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
     }
 
