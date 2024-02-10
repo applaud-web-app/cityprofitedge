72679cbf729a05dbfced0a6df0ebfa1e7b4066ca
devansh9012 <tdevansh099@gmail.com>
2024-02-10 11:43:40 +0530

    new

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
         $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
-        $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
+        // $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
         $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
     }
 
