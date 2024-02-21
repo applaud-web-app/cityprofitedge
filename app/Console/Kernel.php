23d7db4578d71c03ca41e48ce15d8e0833e23df0
devansh9012 <tdevansh099@gmail.com>
2024-02-21 12:40:19 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,11 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
-        $schedule->command('oms-config-rt:command')->everyMinute()->sendOutputTo('command5_output.log');
-        $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
-        $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
-        $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
+        // $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
+        // $schedule->command('oms-config-rt:command')->everyMinute()->sendOutputTo('command5_output.log');
+        $schedule->command('angel_instrument:daily_update')->everyMinute()->sendOutputTo('command2_output.log');
+        // $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
+        // $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
         // $schedule->command('store_market_data:store_data')->everyMinute()->sendOutputTo('command6_output.log');
         // $schedule->command('place_limit_ordre:limitOrder')->everyMinute()->sendOutputTo('command7_output.log');
     }
