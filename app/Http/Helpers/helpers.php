5d934941eca3215d7f1aa14edb7601024f0de983
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-03-29 10:51:17 +0530

    Update helpers.php

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -488,7 +488,11 @@ function allTradeSymbols(){
 }
 
 function allTradeTimeFrames(){
-    return [1,3,5,15,20];
+    return [1,3,5];
+}
+
+function allTradeTimeFramesNew(){
+    return [15];
 }
 
 function strategyNames(){
