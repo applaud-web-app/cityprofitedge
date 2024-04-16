f04e300a2a99ca3cc894e40a1d1a3f6e3f7d20b2
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-04-16 11:40:02 +0530

    w

--- resources/views/templates/basic/user/option-analysis.blade.php
+++ resources/views/templates/basic/user/option-analysis.blade.php
@@ -699,85 +699,85 @@
 
 //   apex charts 2
 
-var options = {
-          series: [{
-            name: "Session Duration",
-            data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10,14]
-          },
-          {
-            name: "Page Views",
-            data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35,45]
-          },
-          {
-            name: 'Total Visits',
-            data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47, 65]
-          }
-        ],
-          chart: {
-          height: 400,
-          foreColor: '#e4e4e4',
-          type: 'line',
-          zoom: {
-            enabled: false
-          },
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          width: [5, 7, 5],
-          curve: 'straight',
-          dashArray: [0, 8, 5]
-        },
+// var options = {
+//           series: [{
+//             name: "Session Duration",
+//             data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10,14]
+//           },
+//           {
+//             name: "Page Views",
+//             data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35,45]
+//           },
+//           {
+//             name: 'Total Visits',
+//             data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47, 65]
+//           }
+//         ],
+//           chart: {
+//           height: 400,
+//           foreColor: '#e4e4e4',
+//           type: 'line',
+//           zoom: {
+//             enabled: false
+//           },
+//         },
+//         dataLabels: {
+//           enabled: false
+//         },
+//         stroke: {
+//           width: [5, 7, 5],
+//           curve: 'straight',
+//           dashArray: [0, 8, 5]
+//         },
        
-        legend: {
-          tooltipHoverFormatter: function(val, opts) {
-            return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
-          }
-        },
-        markers: {
-          size: 0,
-          hover: {
-            sizeOffset: 6
-          }
-        },
-        xaxis: {
-          categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-            '10 Jan', '11 Jan', '12 Jan','13 Jan'
-          ],
-        },
-        tooltip: {
-            enabled: true,
-    theme: 'dark',
-          y: [
-            {
-              title: {
-                formatter: function (val) {
-                  return val + " (mins)"
-                }
-              }
-            },
-            {
-              title: {
-                formatter: function (val) {
-                  return val + " per session"
-                }
-              }
-            },
-            {
-              title: {
-                formatter: function (val) {
-                  return val;
-                }
-              }
-            }
-          ]
-        },
+//         legend: {
+//           tooltipHoverFormatter: function(val, opts) {
+//             return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
+//           }
+//         },
+//         markers: {
+//           size: 0,
+//           hover: {
+//             sizeOffset: 6
+//           }
+//         },
+//         xaxis: {
+//           categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+//             '10 Jan', '11 Jan', '12 Jan','13 Jan'
+//           ],
+//         },
+//         tooltip: {
+//             enabled: true,
+//     theme: 'dark',
+//           y: [
+//             {
+//               title: {
+//                 formatter: function (val) {
+//                   return val + " (mins)"
+//                 }
+//               }
+//             },
+//             {
+//               title: {
+//                 formatter: function (val) {
+//                   return val + " per session"
+//                 }
+//               }
+//             },
+//             {
+//               title: {
+//                 formatter: function (val) {
+//                   return val;
+//                 }
+//               }
+//             }
+//           ]
+//         },
     
-        };
+//         };
 
-        var chart = new ApexCharts(document.querySelector("#apex-analysis-chart2"), options);
-        chart.render();
+//         var chart = new ApexCharts(document.querySelector("#apex-analysis-chart2"), options);
+//         chart.render();
 </script>
 {{-- ce-red.pe-green --}}
 @php
