3678dc47022fb7a5c2a6839ac5347acc93fdd7ef
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-04-27 12:06:12 +0530

    j

--- resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
+++ resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
@@ -200,6 +200,10 @@
                     if($v == "LTP"){
 
                     }else{
+                        $dataLast = \DB::connection('mysql_rm')->table($v)->select('date')->where(['timeframe'=>$timeFrame])->orderBy('id','DESC')->first();
+                        if($dataLast){
+                            $todayDate = $dataLast->date;
+                        }
                         $data = \DB::connection('mysql_rm')->table($v)->select('*')->where(['date'=>$todayDate,'timeframe'=>$timeFrame])->get(); 
                         // if(count($data)==0){
                         //     $data = \DB::connection('mysql_rm')->table($stockName)->select('*')->where(['timeframe'=>$timeFrame])->get();
