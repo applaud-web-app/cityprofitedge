65304afa43b4ad68bdf2a22ee11425c2f87deb78
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-04-27 12:20:37 +0530

    ss

--- resources/views/templates/basic/user/portfolio_top_gainers_ajax_stock.blade.php
+++ resources/views/templates/basic/user/portfolio_top_gainers_ajax_stock.blade.php
@@ -140,7 +140,7 @@
                 
                 @foreach($symbolArr as $v)
                     @php 
-                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO'])){
+                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO','Paper Trade'])){
                         continue;
                     }
                     if($v == "LTP"){
--- resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
+++ resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
@@ -31,7 +31,7 @@
                     <select name="stock_name" class="form--control" id="">
                         <option value="">Select Symbol  Name</option>
                         @foreach ($symbolArr as $v)
-                            @if(!in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO']))
+                            @if(!in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO','Paper Trade']))
                                 <option value="{{$v}}" {{$v==$stockName ? 'selected':''}}>{{$v}}</option>
                              @endif
                         @endforeach
@@ -194,7 +194,7 @@
                 
                 @foreach($symbolArr as $v)
                     @php 
-                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO'])){
+                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO','Paper Trade'])){
                         continue;
                     }
                     if($v == "LTP"){
