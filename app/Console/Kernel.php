56be6c5a489db0c255a568f66918417a5ca0efa2
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-04-30 14:11:12 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,12 +27,12 @@ class Kernel extends ConsoleKernel
 
 
         // New Crons
-        $schedule->command('crudeoil:every_minute')->everyMinute()->sendOutputTo('command10_output.log');
-        $schedule->command('banknifty:every_minute')->everyMinute()->sendOutputTo('command11_output.log');
-        $schedule->command('finifty:every_minute')->everyMinute()->sendOutputTo('command12_output.log');
-        $schedule->command('naturalgas:every_minute')->everyMinute()->sendOutputTo('command13_output.log');
-        $schedule->command('nifty:every_minute')->everyMinute()->sendOutputTo('command14_output.log');
-        $schedule->command('midcpnifty:every_minute')->everyMinute()->sendOutputTo('command15_output.log');
+        // $schedule->command('crudeoil:every_minute')->everyMinute()->sendOutputTo('command10_output.log');
+        // $schedule->command('banknifty:every_minute')->everyMinute()->sendOutputTo('command11_output.log');
+        // $schedule->command('finifty:every_minute')->everyMinute()->sendOutputTo('command12_output.log');
+        // $schedule->command('naturalgas:every_minute')->everyMinute()->sendOutputTo('command13_output.log');
+        // $schedule->command('nifty:every_minute')->everyMinute()->sendOutputTo('command14_output.log');
+        // $schedule->command('midcpnifty:every_minute')->everyMinute()->sendOutputTo('command15_output.log');
 
         // User Home Page
         $schedule->command('angel_top_gainer:every_day')->everyMinute()->sendOutputTo('command16_output.log');
