636f520c74057c015f332ed98533cb794aab4854
devansh9012 <tdevansh099@gmail.com>
2024-05-07 15:50:13 +0530

    sds

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -195,12 +195,10 @@ class UserController extends Controller
         $stock_name = "";
         $timeframe = "5";
         $currentDate = date('Y-m-d');
-        $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC');
+        $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC')->orderby('timestamp','DESC');
         if($request->stock_name != NULL){
             $stock_name = $request->stock_name;
             $greekSentiments =  $greekSentiments->where('symbol',$stock_name);
-        }else{
-            $greekSentiments =  $greekSentiments->where('date',$currentDate);
         }
         if($request->timeframe != NULL){
             $timeframe = $request->timeframe;
@@ -209,7 +207,7 @@ class UserController extends Controller
         $greekSentiments =  $greekSentiments->paginate(30);
 
         if($greekSentiments == NULL){
-            $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC');
+            $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC')->orderby('timestamp','DESC');
             if($request->stock_name != NULL){
                 $stock_name = $request->stock_name;
             }
@@ -233,7 +231,7 @@ class UserController extends Controller
         $stock_name = "";
         $timeframe = "5";
         $currentDate = date('Y-m-d');
-        $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->where('date',$currentDate)->orderBy('date','DESC');
+        $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->where('date',$currentDate)->orderBy('date','DESC')->orderby('timestamp','DESC');
         if($request->stock_name != NULL){
             $stock_name = $request->stock_name;
             $greekSentiments =  $greekSentiments->where('symbol',$stock_name);
@@ -245,7 +243,7 @@ class UserController extends Controller
         $greekSentiments =  $greekSentiments->paginate(30);
 
         if($greekSentiments == NULL){
-            $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC');
+            $greekSentiments = \DB::connection('mysql_rm')->table('IV Theta Sentiments')->select('*')->orderBy('date','DESC')->orderby('timestamp','DESC');
             if($request->stock_name != NULL){
                 $stock_name = $request->stock_name;
             }
