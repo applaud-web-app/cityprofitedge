11f3cfc3bc42f546a99b0363695d72ce60ddf147
devansh9012 <tdevansh099@gmail.com>
2024-05-08 10:35:23 +0530

    dsd

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -47,11 +47,11 @@ class SiteController extends Controller
         $shortBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','short')->paginate(5);
         $coveringBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','covering')->paginate(5);
         $unWindingBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','unwinding')->paginate(5);
-        $todayDate = date('d-m-Y');
-        $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->where('date',$todayDate)->get(); 
-        if(!count($fdProData)){
+        // $todayDate = date('d-m-Y');
+        // $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->where('date',$todayDate)->get(); 
+        // if(!count($fdProData)){
             $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->limit(5)->get(); 
-        }
+        // }
         $reference = @$_GET['reference'];
         if ($reference) {
             session()->put('reference', $reference);
@@ -77,11 +77,11 @@ class SiteController extends Controller
         $shortBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','short')->paginate(5);
         $coveringBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','covering')->paginate(5);
         $unWindingBuildUp = OiBuildUp::orderBy('id','DESC')->where('type','unwinding')->paginate(5);
-        $todayDate = date('d-m-Y',strtotime('-1Day'));
-        $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->where('date',$todayDate)->get(); 
-        if(!count($fdProData)){
+        // $todayDate = date('d-m-Y',strtotime('-1Day'));
+        // $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->where('date',$todayDate)->get(); 
+        // if(!count($fdProData)){
             $fdProData = \DB::connection('mysql_rm')->table('FII DII PRO')->select('*')->where('client_type','!=','TOTAL')->limit(5)->get(); 
-        }
+        // }
         $reference = @$_GET['reference'];
         if ($reference) {
             session()->put('reference', $reference);
