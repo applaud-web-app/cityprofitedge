f1f1bb35d78559ded6c3f7ed44a127398ae7a551
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-05-24 14:10:11 +0530

    f

--- app/Console/Commands/WatchListData.php
+++ app/Console/Commands/WatchListData.php
@@ -39,17 +39,15 @@ class WatchListData extends Command
         $symbolArr = allTradeSymbols();
         $todayDate = date("Y-m-d");
         
-        $MCXpayload = [];
-        $NFOpayload = [];
+        
 
         foreach ($symbolArr as $key => $v) {
-
+            $MCXpayload = [];
+            $NFOpayload = [];
             $extraTable = ['FII DII PRO','LTP','IV Theta Sentiments','MATCH-DELTA','MATCH-THETA','MATCH-PREMIUM','MATCH-IV','Paper Trade','Predictions'];
-
             if(in_array($v,$extraTable)){
                 continue;
             }
-
             $data = \DB::connection('mysql_rm')->table($v)->select('ce as symbol_ce','pe as symbol_pe','ce_token as token_ce','pe_token as token_pe','exchange')->orderBy('id','DESC')->get(); 
 
             foreach ($data as $key => $value) {
@@ -61,95 +59,164 @@ class WatchListData extends Command
                     array_push($NFOpayload,$value->token_pe);
                 }
             }
+
+            if(!empty($MCXpayload)){
+                $mcxChunkData = array_chunk($MCXpayload,50,true);
+                foreach($mcxChunkData as $val){
+                    $responseData = [];
+                    $finalpayLoad = [
+                        'MCX'=>array_map('json_encode', $val)
+                    ];
+                    $payload = json_encode($finalpayLoad,true);
+                    $respond = $this->getWatchListRecords($payload);
+                    if(isset($respond)){
+                        if($respond['status'] == true){
+                            $responseData = $respond['data']['fetched'];
+                             if(count($responseData)){
+                                foreach ($responseData as $key => $value) {
+                                        $wishlist = new WishlistData;
+                                        $wishlist->symbol_name = $value['tradingSymbol'];
+                                        $wishlist->symbolToken = $value['symbolToken'];
+                                        $wishlist->exchange = $value['exchange'];
+                                        $wishlist->ltp = $value['ltp'];
+                                        $wishlist->open = $value['open'];
+                                        $wishlist->high = $value['high'];
+                                        $wishlist->low = $value['low'];
+                                        $wishlist->close = $value['close'];
+                                        $wishlist->lastTradeQty = $value['lastTradeQty'];
+                                        $wishlist->exchFeedTime = $value['exchFeedTime'];
+                                        $wishlist->exchTradeTime = $value['exchTradeTime'];
+                                        $wishlist->netChange = $value['netChange'];
+                                        $wishlist->percentChange = $value['percentChange'];
+                                        $wishlist->avgPrice = $value['avgPrice'];
+                                        $wishlist->tradeVolume = $value['tradeVolume'];
+                                        $wishlist->opnInterest = $value['opnInterest'];
+                                        $wishlist->lowerCircuit = $value['lowerCircuit'];
+                                        $wishlist->upperCircuit = $value['upperCircuit'];
+                                        $wishlist->totBuyQuan = $value['totBuyQuan'];
+                                        $wishlist->totSellQuan = $value['totSellQuan'];
+                                        $wishlist->WeekLow52 = $value['52WeekLow'];
+                                        $wishlist->WeekHigh52 = $value['52WeekHigh'];
+                                        $wishlist->save();
+    
+                                        $type = substr($value['tradingSymbol'],-2,2);
+                                        if($type == "CE"){
+    
+                                            // FOR MATCH DELTA CE SYMBOLS
+                                            $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH THETA CE SYMBOLS
+                                            $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH PREMIUM CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH IV CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                        }else if($type == "PE"){
+                                            // FOR MATCH DELTA PE SYMBOLS
+                                            $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH THETA CE SYMBOLS
+                                            $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH PREMIUM CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH IV CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                        }
+                                }  
+                            }   
+                        }
+                    }
+                    sleep(1);
+                }
+    
+            }
+    
+            if(!empty($NFOpayload)){
+                $nfoChunkData = array_chunk($NFOpayload,50,true);
+                foreach($nfoChunkData as $val){
+                    $responseData = [];
+                        $finalpayLoad = [
+                        'NFO'=>array_map('json_encode', $val)
+                    ];
+                    $payload = json_encode($finalpayLoad,true);
+                    $respond = $this->getWatchListRecords($payload);
+                    if(isset($respond)){
+                        if($respond['status'] == true){
+                            $responseData = $respond['data']['fetched'];
+                             if(count($responseData)){
+                                foreach ($responseData as $key => $value) {
+                                        $wishlist = new WishlistData;
+                                        $wishlist->symbol_name = $value['tradingSymbol'];
+                                        $wishlist->symbolToken = $value['symbolToken'];
+                                        $wishlist->exchange = $value['exchange'];
+                                        $wishlist->ltp = $value['ltp'];
+                                        $wishlist->open = $value['open'];
+                                        $wishlist->high = $value['high'];
+                                        $wishlist->low = $value['low'];
+                                        $wishlist->close = $value['close'];
+                                        $wishlist->lastTradeQty = $value['lastTradeQty'];
+                                        $wishlist->exchFeedTime = $value['exchFeedTime'];
+                                        $wishlist->exchTradeTime = $value['exchTradeTime'];
+                                        $wishlist->netChange = $value['netChange'];
+                                        $wishlist->percentChange = $value['percentChange'];
+                                        $wishlist->avgPrice = $value['avgPrice'];
+                                        $wishlist->tradeVolume = $value['tradeVolume'];
+                                        $wishlist->opnInterest = $value['opnInterest'];
+                                        $wishlist->lowerCircuit = $value['lowerCircuit'];
+                                        $wishlist->upperCircuit = $value['upperCircuit'];
+                                        $wishlist->totBuyQuan = $value['totBuyQuan'];
+                                        $wishlist->totSellQuan = $value['totSellQuan'];
+                                        $wishlist->WeekLow52 = $value['52WeekLow'];
+                                        $wishlist->WeekHigh52 = $value['52WeekHigh'];
+                                        $wishlist->save();
+    
+                                        $type = substr($value['tradingSymbol'],-2,2);
+                                        if($type == "CE"){
+    
+                                            // FOR MATCH DELTA CE SYMBOLS
+                                            $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH THETA CE SYMBOLS
+                                            $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH PREMIUM CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH IV CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+    
+                                        }else if($type == "PE"){
+                                            // FOR MATCH DELTA PE SYMBOLS
+                                            $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH THETA CE SYMBOLS
+                                            $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH PREMIUM CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                            // FOR MATCH IV CE SYMBOLS
+                                            $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+    
+                                        }
+                                }  
+                            }   
+                        }
+                    }
+                    sleep(1);
+                }
+                
+    
+            }
         }
 
-        $payload = [
-            'MCX'=>$MCXpayload,
-            'NFO'=>$NFOpayload
-        ];
-
-        $chunk['MCX'] = array_chunk($payload['MCX'],50,true);
-        $chunk['NFO'] = array_chunk($payload['NFO'],50,true);
-
-        $responseData = [];
-        $index = 0;
-
-        foreach ($chunk as $key => $value) {
-            $finalpayLoad = [
-                $key=>array_map('json_encode', $value[$index])
-            ];
-            $payload = json_encode($finalpayLoad,true);
-            $respond = $this->getWatchListRecords($payload);
-            if(isset($respond)){
-                if($respond['status'] == true){
-                    //$responseData = $respond['data']['fetched'];
-                    array_push($responseData,$respond['data']['fetched']);
-                }
-            }
-            sleep(1);
-        }     
-
-        // Insert Data To Watchlist
-        if(count($responseData)){
-            foreach ($responseData as $key => $respond) {
-                foreach ($respond as $key => $value) {
-                    $wishlist = new WishlistData;
-                    $wishlist->symbol_name = $value['tradingSymbol'];
-                    $wishlist->symbolToken = $value['symbolToken'];
-                    $wishlist->exchange = $value['exchange'];
-                    $wishlist->ltp = $value['ltp'];
-                    $wishlist->open = $value['open'];
-                    $wishlist->high = $value['high'];
-                    $wishlist->low = $value['low'];
-                    $wishlist->close = $value['close'];
-                    $wishlist->lastTradeQty = $value['lastTradeQty'];
-                    $wishlist->exchFeedTime = $value['exchFeedTime'];
-                    $wishlist->exchTradeTime = $value['exchTradeTime'];
-                    $wishlist->netChange = $value['netChange'];
-                    $wishlist->percentChange = $value['percentChange'];
-                    $wishlist->avgPrice = $value['avgPrice'];
-                    $wishlist->tradeVolume = $value['tradeVolume'];
-                    $wishlist->opnInterest = $value['opnInterest'];
-                    $wishlist->lowerCircuit = $value['lowerCircuit'];
-                    $wishlist->upperCircuit = $value['upperCircuit'];
-                    $wishlist->totBuyQuan = $value['totBuyQuan'];
-                    $wishlist->totSellQuan = $value['totSellQuan'];
-                    $wishlist->WeekLow52 = $value['52WeekLow'];
-                    $wishlist->WeekHigh52 = $value['52WeekHigh'];
-                    $wishlist->save();
-
-                    $type = substr($value['tradingSymbol'],-2,2);
-                    if($type == "CE"){
-
-                        // FOR MATCH DELTA CE SYMBOLS
-                        $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH THETA CE SYMBOLS
-                        $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH PREMIUM CE SYMBOLS
-                        $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH IV CE SYMBOLS
-                        $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
-
-                    }else if($type == "PE"){
-                        // FOR MATCH DELTA PE SYMBOLS
-                        $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH THETA CE SYMBOLS
-                        $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH PREMIUM CE SYMBOLS
-                        $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
-
-                        // FOR MATCH IV CE SYMBOLS
-                        $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
-
-                    }
-                }
-            }  
-        }   
+        
 
     }
 }
---
+++ app/Console/Commands/WatchlistDataOld.php
@@ -0,0 +1,155 @@
+<?php
+
+// namespace App\Console\Commands;
+
+// use Illuminate\Console\Command;
+// use App\Traits\AngelApiAuth;
+// use App\Models\WatchList;
+// use App\Models\WishlistData;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Artisan;
+
+
+// class WatchListData extends Command
+// {
+//     use AngelApiAuth;
+//     /**
+//      * The name and signature of the console command.
+//      *
+//      * @var string
+//      */
+//     protected $signature = 'watch-list-data:every_minute';
+
+//     /**
+//      * The console command description.
+//      *
+//      * @var string
+//      */
+//     protected $description = 'Command description';
+
+//     /**
+//      * Execute the console command.
+//      *
+//      * @return int
+//      */
+//     public function handle()
+//     {
+//         // Artisan::call('queue:work');
+//         set_time_limit(0);
+//         $symbolArr = allTradeSymbols();
+//         $todayDate = date("Y-m-d");
+        
+//         $MCXpayload = [];
+//         $NFOpayload = [];
+
+//         foreach ($symbolArr as $key => $v) {
+
+//             $extraTable = ['FII DII PRO','LTP','IV Theta Sentiments','MATCH-DELTA','MATCH-THETA','MATCH-PREMIUM','MATCH-IV','Paper Trade','Predictions'];
+
+//             if(in_array($v,$extraTable)){
+//                 continue;
+//             }
+
+//             $data = \DB::connection('mysql_rm')->table($v)->select('ce as symbol_ce','pe as symbol_pe','ce_token as token_ce','pe_token as token_pe','exchange')->orderBy('id','DESC')->get(); 
+
+//             foreach ($data as $key => $value) {
+//                 if($value->exchange == "MCX"){
+//                     array_push($MCXpayload,$value->token_ce);
+//                     array_push($MCXpayload,$value->token_pe);
+//                 }else if($value->exchange == "NFO"){
+//                     array_push($NFOpayload,$value->token_ce);
+//                     array_push($NFOpayload,$value->token_pe);
+//                 }
+//             }
+//         }
+
+//         $payload = [
+//             'MCX'=>$MCXpayload,
+//             'NFO'=>$NFOpayload
+//         ];
+
+//         $chunk['MCX'] = array_chunk($payload['MCX'],50,true);
+//         $chunk['NFO'] = array_chunk($payload['NFO'],50,true);
+
+//         $responseData = [];
+//         $index = 0;
+
+//         foreach ($chunk as $key => $value) {
+//             $finalpayLoad = [
+//                 $key=>array_map('json_encode', $value[$index])
+//             ];
+//             $payload = json_encode($finalpayLoad,true);
+//             $respond = $this->getWatchListRecords($payload);
+//             if(isset($respond)){
+//                 if($respond['status'] == true){
+//                     //$responseData = $respond['data']['fetched'];
+//                     array_push($responseData,$respond['data']['fetched']);
+//                 }
+//             }
+//             sleep(1);
+//         }     
+
+//         // Insert Data To Watchlist
+//         if(count($responseData)){
+//             foreach ($responseData as $key => $respond) {
+//                 foreach ($respond as $key => $value) {
+//                     $wishlist = new WishlistData;
+//                     $wishlist->symbol_name = $value['tradingSymbol'];
+//                     $wishlist->symbolToken = $value['symbolToken'];
+//                     $wishlist->exchange = $value['exchange'];
+//                     $wishlist->ltp = $value['ltp'];
+//                     $wishlist->open = $value['open'];
+//                     $wishlist->high = $value['high'];
+//                     $wishlist->low = $value['low'];
+//                     $wishlist->close = $value['close'];
+//                     $wishlist->lastTradeQty = $value['lastTradeQty'];
+//                     $wishlist->exchFeedTime = $value['exchFeedTime'];
+//                     $wishlist->exchTradeTime = $value['exchTradeTime'];
+//                     $wishlist->netChange = $value['netChange'];
+//                     $wishlist->percentChange = $value['percentChange'];
+//                     $wishlist->avgPrice = $value['avgPrice'];
+//                     $wishlist->tradeVolume = $value['tradeVolume'];
+//                     $wishlist->opnInterest = $value['opnInterest'];
+//                     $wishlist->lowerCircuit = $value['lowerCircuit'];
+//                     $wishlist->upperCircuit = $value['upperCircuit'];
+//                     $wishlist->totBuyQuan = $value['totBuyQuan'];
+//                     $wishlist->totSellQuan = $value['totSellQuan'];
+//                     $wishlist->WeekLow52 = $value['52WeekLow'];
+//                     $wishlist->WeekHigh52 = $value['52WeekHigh'];
+//                     $wishlist->save();
+
+//                     $type = substr($value['tradingSymbol'],-2,2);
+//                     if($type == "CE"){
+
+//                         // FOR MATCH DELTA CE SYMBOLS
+//                         $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH THETA CE SYMBOLS
+//                         $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH PREMIUM CE SYMBOLS
+//                         $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH IV CE SYMBOLS
+//                         $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('ce',$value['tradingSymbol'])->update(['ce_ltp'=>$value['ltp']]);
+
+//                     }else if($type == "PE"){
+//                         // FOR MATCH DELTA PE SYMBOLS
+//                         $matchDelta = \DB::connection('mysql_rm')->table('MATCH-DELTA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH THETA CE SYMBOLS
+//                         $matchTheta = \DB::connection('mysql_rm')->table('MATCH-THETA')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH PREMIUM CE SYMBOLS
+//                         $matchPremium = \DB::connection('mysql_rm')->table('MATCH-PREMIUM')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+
+//                         // FOR MATCH IV CE SYMBOLS
+//                         $matchPremium = \DB::connection('mysql_rm')->table('MATCH-IV')->where('pe',$value['tradingSymbol'])->update(['pe_ltp'=>$value['ltp']]);
+
+//                     }
+//                 }
+//             }  
+//         }   
+
+//     }
+// }
