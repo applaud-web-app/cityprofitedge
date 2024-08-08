735cfd6f455ac0f8252031decf520edbe09b4055
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-08-08 17:04:21 +0530

    d

--- resources/views/templates/basic/user/paper-x-factor.blade.php
+++ resources/views/templates/basic/user/paper-x-factor.blade.php
@@ -1,6 +1,6 @@
 @extends($activeTemplate.'layouts.master')
 @section('content')
-<section class="pt-100 pb-100">
+<section class="pt-50 pb-50">
 
     <div class="container-fluid" id="pst_hre">
         <div class="row" >
