aa7f0138715e5dc1fb13c066963c37cf46002f7b
binnyrawat <rawat8457@gmail.com>
2024-08-09 23:03:34 +0530

    Update paper-x-factor.blade.php

--- resources/views/templates/basic/user/paper-x-factor.blade.php
+++ resources/views/templates/basic/user/paper-x-factor.blade.php
@@ -1,7 +1,7 @@
 @extends($activeTemplate.'layouts.master')
 @section('content')
 <section class="pt-50 pb-50">
-    <div class="container content-container" id="pst_hre">
+    <div class="container content-container">
         <!-- Form to filter by date -->
         <form action="{{ url()->current() }}" method="GET" class="transparent-form mb-3">
             <div class="row">
