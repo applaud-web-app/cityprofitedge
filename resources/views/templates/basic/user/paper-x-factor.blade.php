bb817d85608b0ce8319f6910b5275af09ecc830f
binnyrawat <rawat8457@gmail.com>
2024-08-09 20:41:11 +0530

    Update paper-x-factor.blade.php

--- resources/views/templates/basic/user/paper-x-factor.blade.php
+++ resources/views/templates/basic/user/paper-x-factor.blade.php
@@ -50,7 +50,7 @@
                                     </thead>
                                     <tbody>
                                         @php
-                                            dd($data);
+                                            dd($value);
                                             $data = json_decode($value->data,true);
                                             $revArr = array_reverse($data);
                                             $fData = array_slice($revArr,0,5);
