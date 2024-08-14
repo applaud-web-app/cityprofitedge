69adf91bd5eaaa364ec085f1121d2146ed65a70c
devansh9012 <tdevansh099@gmail.com>
2024-08-14 17:18:01 +0530

    xcv

--- resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
+++ resources/views/templates/basic/user/portfolio_top_gainers_stock.blade.php
@@ -194,7 +194,7 @@
                 
                 @foreach($symbolArr as $v)
                     @php 
-                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO','Paper Trade','LTP','IV Theta Sentiments','MATCH-DELTA','MATCH-IV','MATCH-PREMIUM','MATCH-THETA','IV Theta Sentiments','PaperOrderPlacement'])){
+                    if(in_array($v,['CRUDEOIL','BANKNIFTY','FINNIFTY','SILVER','NIFTY','MIDCPNIFTY','NATURALGAS','SILVER','GOLD','FII DII PRO','Paper Trade','LTP','IV Theta Sentiments','MATCH-DELTA','MATCH-IV','MATCH-PREMIUM','MATCH-THETA','IV Theta Sentiments','PaperOrderPlacement','date'])){
                         continue;
                     }
                     if($v == "LTP"){
