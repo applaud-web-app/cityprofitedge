b270c9eae94c111d1cd04803e999310270df3864
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-08-29 13:36:41 +0530

    fgfb

--- app/Console/Commands/UpdatePortfoilioLtp.php
+++ app/Console/Commands/UpdatePortfoilioLtp.php
@@ -52,6 +52,7 @@ class UpdatePortfoilioLtp extends Command
 
       
         $newArray = array_unique($responseData);
+        dd($newArray);
     
         if (count($newArray)) {
            $fetchData =  array_chunk($newArray,50);
--- app/Traits/AngelApiAuth.php
+++ app/Traits/AngelApiAuth.php
@@ -1342,7 +1342,6 @@ trait AngelApiAuth
             ),
             ));
             $response = curl_exec($curl);
-            dd($response);
             $err = curl_error($curl);
             curl_close($curl);
             if ($err) {
