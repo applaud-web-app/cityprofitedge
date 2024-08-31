d0ec4c1e1205804427b478c301067bb4be97602c
devansh9012 <tdevansh099@gmail.com>
2024-08-31 14:34:52 +0530

    dsf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,11 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
-        $schedule->command('oms-config-rt:command')->everyMinute()->sendOutputTo('command5_output.log');
-        $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
-        $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
-        $schedule->command('place_limit_ordre:limitOrder')->everyMinute()->sendOutputTo('command7_output.log');
+        // $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
+        // $schedule->command('oms-config-rt:command')->everyMinute()->sendOutputTo('command5_output.log');
+        // $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
+        // $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
+        // $schedule->command('place_limit_ordre:limitOrder')->everyMinute()->sendOutputTo('command7_output.log');
 
         // NO NEED (REMOVED)
         // $schedule->command('angleHistorical:every_minute')->everyMinute()->sendOutputTo('command3_output.log');
@@ -35,18 +35,18 @@ class Kernel extends ConsoleKernel
         // $schedule->command('midcpnifty:every_minute')->everyMinute()->sendOutputTo('command15_output.log');
 
         // User Home Page
-        $schedule->command('angel_top_gainer:every_day')->everyMinute()->sendOutputTo('command16_output.log');
-        $schedule->command('angel_top_loser:every_day')->everyMinute()->sendOutputTo('command17_output.log');
-        $schedule->command('angel_short_oi_buildup:every_day')->everyMinute()->sendOutputTo('command18_output.log');
-        $schedule->command('angel_long_oi_buildup:every_day')->everyMinute()->sendOutputTo('command19_output.log');
-        $schedule->command('angel_short_covering_buildup:every_day')->everyMinute()->sendOutputTo('command20_output.log');
-        $schedule->command('angel_long_unwinding_buildup:every_day')->everyMinute()->sendOutputTo('command21_output.log');
-        $schedule->command('angel_pcr:every_day')->everyMinute()->sendOutputTo('command23_output.log');
+        // $schedule->command('angel_top_gainer:every_day')->everyMinute()->sendOutputTo('command16_output.log');
+        // $schedule->command('angel_top_loser:every_day')->everyMinute()->sendOutputTo('command17_output.log');
+        // $schedule->command('angel_short_oi_buildup:every_day')->everyMinute()->sendOutputTo('command18_output.log');
+        // $schedule->command('angel_long_oi_buildup:every_day')->everyMinute()->sendOutputTo('command19_output.log');
+        // $schedule->command('angel_short_covering_buildup:every_day')->everyMinute()->sendOutputTo('command20_output.log');
+        // $schedule->command('angel_long_unwinding_buildup:every_day')->everyMinute()->sendOutputTo('command21_output.log');
+        // $schedule->command('angel_pcr:every_day')->everyMinute()->sendOutputTo('command23_output.log');
 
 
         // WATCH LIST CRONS
-        $schedule->command('watch-list-data:every_minute')->everyMinute()->sendOutputTo('command25_output.log');
-        $schedule->command('paper_trading:every_minute')->everyMinute()->sendOutputTo('command26_output.log');
+        // $schedule->command('watch-list-data:every_minute')->everyMinute()->sendOutputTo('command25_output.log');
+        // $schedule->command('paper_trading:every_minute')->everyMinute()->sendOutputTo('command26_output.log');
         $schedule->command('update_portfolio_ltp:every_minute')->everyMinute()->sendOutputTo('command29_output.log');
         // $schedule->command('store_strength_data:every_fifteen_minute')->everyFifteenMinutes()->sendOutputTo('command27_output.log');
 
