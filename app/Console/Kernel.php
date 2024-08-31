8d75b8d8919a5a3e8806632e9689169edcf2ee70
devansh9012 <tdevansh099@gmail.com>
2024-08-31 15:21:12 +0530

    sdf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('oms-config:command')->everyMinute()->sendOutputTo('command1_output.log');
         // $schedule->command('oms-config-rt:command')->everyMinute()->sendOutputTo('command5_output.log');
-        // $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
+        $schedule->command('angel_instrument:daily_update')->dailyAt('9:00')->sendOutputTo('command2_output.log');
         // $schedule->command('zerodha_instrument:insert')->dailyAt('08:30')->sendOutputTo('command4_output.log');
         // $schedule->command('place_limit_ordre:limitOrder')->everyMinute()->sendOutputTo('command7_output.log');
 
@@ -47,7 +47,7 @@ class Kernel extends ConsoleKernel
         // WATCH LIST CRONS
         // $schedule->command('watch-list-data:every_minute')->everyMinute()->sendOutputTo('command25_output.log');
         // $schedule->command('paper_trading:every_minute')->everyMinute()->sendOutputTo('command26_output.log');
-        // $schedule->command('update_portfolio_ltp:every_minute')->everyMinute()->sendOutputTo('command29_output.log');
+        $schedule->command('update_portfolio_ltp:every_minute')->everyMinute()->sendOutputTo('command29_output.log');
         // $schedule->command('store_strength_data:every_fifteen_minute')->everyFifteenMinutes()->sendOutputTo('command27_output.log');
 
     }
