a8339001f00ce3579f0469af28538985173f7423
binnyrawat <rawat8457@gmail.com>
2024-10-31 13:56:32 +0530

    Update paper-x-factor-combined.blade.php

--- resources/views/templates/basic/user/paper-x-factor-combined.blade.php
+++ resources/views/templates/basic/user/paper-x-factor-combined.blade.php
@@ -34,7 +34,6 @@
                 <div class="col-lg-12">
                     
                     <div class="custom--card">
-                        <div class="card-header"></div>
                         <div class="card-body p-0">
                             <div class="table-responsive--md table-responsive">
                                 <table class="table custom--table text-nowrap">
