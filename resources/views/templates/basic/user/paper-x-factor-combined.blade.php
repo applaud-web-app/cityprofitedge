d34f0716dcb824bb9575b25ff6ab4c2c84ea7f42
binnyrawat <rawat8457@gmail.com>
2024-10-31 15:09:04 +0530

    Update paper-x-factor-combined.blade.php

--- resources/views/templates/basic/user/paper-x-factor-combined.blade.php
+++ resources/views/templates/basic/user/paper-x-factor-combined.blade.php
@@ -39,7 +39,7 @@
                                 <table class="table custom--table text-nowrap">
                                     <thead>
                                          <tr style="background:#0d222b;font-size:24px;">
-                                             <th>Last Updated: {{date("d M Y H:i:s",strtotime($value[0]->updated_at))}}</th>
+                                             <th>Last Updated: {{date("d M Y H:i:s",strtotime($value->updated_at))}}</th>
                                             <th colspan="6" style="font-size:16px;" class="text-white text-end">Investment Amount</th>
                                             <th class="text-success" style="font-size:16px;">
                                                 {{ $value->total_investment }}
