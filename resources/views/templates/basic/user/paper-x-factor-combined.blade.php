0075b7ce96447d0f2dd95e1e365617b525226f01
binnyrawat <rawat8457@gmail.com>
2024-10-31 12:57:54 +0530

    Update paper-x-factor-combined.blade.php

--- resources/views/templates/basic/user/paper-x-factor-combined.blade.php
+++ resources/views/templates/basic/user/paper-x-factor-combined.blade.php
@@ -14,56 +14,46 @@
                 <div class="col-lg-3 form-group">
                     <label for="factor_date">Choose Date</label>
                     <input type="date" name="factor_date" class="form--control" id="factor_date" 
-                           value="{{ request('factor_date') }}" max="{{ date('Y-m-d') }}" />
+                           value="{{ $today }}" max="{{ date('Y-m-d') }}" />
                 </div>
                 <div class="col-lg-2 form-group mt-auto">
                     <button class="btn btn--base w-100" type="submit"><i class="las la-filter"></i> Filter</button>
                 </div>
                 <div class="col-lg-2 col-md-3 col-6 form-group mt-auto">
-                    <a href="{{ url('/user/paper-x-factor') }}" class="btn btn--base w-100"><i class="las la-redo-alt"></i> Refresh</a>
+                    <a href="{{ url('/user/paper-x-factor-combined') }}" class="btn btn--base w-100"><i class="las la-redo-alt"></i> Refresh</a>
                 </div>
             </div>
         </form>
 
         <div id="pst_hre">
-          @php
-            dd($paperFactor);
-          @endphp
         @forelse($paperFactor as $value)
             <div class="row mb-3">
                 <div class="col-lg-12">
                     
                     <div class="custom--card">
-                        <div class="card-header">{{$value->symbol}}</div>
+                        <div class="card-header"></div>
                         <div class="card-body p-0">
                             <div class="table-responsive--md table-responsive">
                                 <table class="table custom--table text-nowrap">
                                     <thead>
                                         <tr>
                                             <th>@lang('TIME')</th>
-                                            <th>@lang('CE SYMBOL')</th>
-                                            <th>@lang('CE CLOSE')</th>
-                                            <th>@lang('FINAL CE')</th>
-                                            <th>@lang('CE QTY LOTS')</th>
-                                            <th>@lang('CE ENTRY PRICE')</th>
-                                            <th>@lang('CE EXIT PRICE')</th>
-                                            <th>@lang('CE PROFIT')</th>
-                                            <th>@lang('PE SYMBOL')</th>
-                                            <th>@lang('PE CLOSE')</th>
-                                            <th>@lang('FINAL PE')</th>
-                                            <th>@lang('PE QTY LOTS')</th>
-                                            <th>@lang('PE ENTRY PRICE')</th>
-                                            <th>@lang('PE EXIT PRICE')</th>
-                                            <th>@lang('PE PROFIT')</th>
-                                            <th>@lang('STRATEGY PROFIT')</th>
-                                            <th>@lang('TRADE STATUS')</th>
+                                            <th>@lang('TXN Type')</th>
+                                            <th>@lang('Symbol')</th>
+                                            <th>@lang('LotSize')</th>
+                                            <th>@lang('QTY')</th>
+                                            <th>@lang('Investment')</th>
+                                            <th>@lang('Close Price')</th>
+                                            <th>@lang('Entry Price')</th>
+                                            <th>@lang('Current Value')</th>
+                                            <th>@lang('Profit')</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @php
-                                            $data = json_decode($value->data,true);
+                                            $data = json_decode($value->json_data,true);
                                             $revArr = array_reverse($data);
-                                            
+                                            dd($revArr);
                                             $fData = $isFiltered == 0 ? array_slice($revArr,0,5) : $revArr;
                                             $totalItems = 0;
                                             $itemsPerPage = 100;
