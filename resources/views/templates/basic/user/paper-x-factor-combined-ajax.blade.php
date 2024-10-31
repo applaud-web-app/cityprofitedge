80278f34f0a8908e435cd58f311ceb15d98694e3
binnyrawat <rawat8457@gmail.com>
2024-10-31 15:15:41 +0530

    Update paper-x-factor-combined-ajax.blade.php

--- resources/views/templates/basic/user/paper-x-factor-combined-ajax.blade.php
+++ resources/views/templates/basic/user/paper-x-factor-combined-ajax.blade.php
@@ -8,7 +8,7 @@
                                 <table class="table custom--table text-nowrap">
                                     <thead>
                                          <tr style="background:#0d222b;">
-                                             <th style="font-size:16px;">Last Updated: {{date("d M Y H:i:s",strtotime($value->updated_at))}}</th>
+                                             <th style="font-size:12px;">Last Updated: {{date("d M Y H:i:s",strtotime($value->updated_at))}}</th>
                                             <th colspan="6" style="font-size:16px;" class="text-white text-end">Investment Amount</th>
                                             <th class="text-success" style="font-size:16px;">
                                                 {{ $value->total_investment }}
