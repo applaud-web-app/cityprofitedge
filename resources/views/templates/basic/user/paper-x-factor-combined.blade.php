2172cf78f9f1d3df237ad28573a0dca9af3009c3
binnyrawat <rawat8457@gmail.com>
2024-10-31 14:32:13 +0530

    Update paper-x-factor-combined.blade.php

--- resources/views/templates/basic/user/paper-x-factor-combined.blade.php
+++ resources/views/templates/basic/user/paper-x-factor-combined.blade.php
@@ -101,13 +101,15 @@
                                 echo '<nav class="mt-3 justify-content-end d-flex">
                                         <ul class="pagination mb-0">';
                             @endphp
-                            @for($i = 1; $i <= $totalPages; $i++)
-                                @if($i == $currentPage)
-                                    <li class="page-item active" aria-current="page"><span class="page-link">{{$i}}</span></li>
-                                @else
-                                <li class="page-item"><a class="page-link" href="{{url('user/paper-x-factor?factor_symbol='.request('factor_symbol').'&factor_date='.request('factor_date').'&page='.$i.'')}}">{{$i}}</a></li>
-                                @endif
-                            @endfor
+                            @if($totalPages > 1)                
+                                @for($i = 1; $i <= $totalPages; $i++)
+                                    @if($i == $currentPage)
+                                        <li class="page-item active" aria-current="page"><span class="page-link">{{$i}}</span></li>
+                                    @else
+                                    <li class="page-item"><a class="page-link" href="{{url('user/paper-x-factor?factor_symbol='.request('factor_symbol').'&factor_date='.request('factor_date').'&page='.$i.'')}}">{{$i}}</a></li>
+                                    @endif
+                                @endfor
+                            @endif    
                             @php     
                                 echo '  </ul>
                                     </nav>';
