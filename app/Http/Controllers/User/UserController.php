d56a75984b40bb0e521bc620390feec46af0f8d6
binnyrawat <rawat8457@gmail.com>
2024-11-09 16:18:08 +0530

    Update UserController.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1915,7 +1915,7 @@ class UserController extends Controller
 
         // For Chart 1
         $data1 = \DB::connection('mysql_rm')->table($table1)->select('*')->where('timeframe',$timeFrame1)->get();
-           dd($data5);
+           dd($data1);
         // For Chart 2
         $data2 = \DB::connection('mysql_rm')->table($table2)->select('*')->where('timeframe',$timeFrame2)->get();
         // For Chart 3
